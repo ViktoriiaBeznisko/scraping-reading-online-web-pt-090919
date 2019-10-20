d3101a105f3c53382b71b372cd18c1cbcdd77841
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-20 00:19:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,11 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
 # puts doc.css(".headline-26OIBN")
-#doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-course = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+# doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
-course.each{|course| puts course.text.strip}
+courses.each do |course|
+  puts course.text.strip
 end
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
